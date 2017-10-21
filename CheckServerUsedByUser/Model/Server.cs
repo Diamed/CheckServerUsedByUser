a98f4aebbd697e467016f16142570a9a4bad38e8
Dmitriy Bobrovskiy <apollondiamed@gmail.com>
2017-10-21 15:43:05 +0500

    Creating main functionality. Created classes Server and User.

---
+++ CheckServerUsedByUser/Model/Server.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace CheckServerUsedByUser.Model
+{
+    public sealed class Server
+    {
+        private IPAddress _ipAddress;
+        public IPAddress IPAddress
+        {
+            get => _ipAddress;
+            set
+            {
+                _ipAddress = value;
+                OnPropertyChanged(nameof(IPAddress));
+            }
+        }
+
+        private User _activeUser;
+        public User ActiveUser
+        {
+            get => _activeUser;
+            set
+            {
+                _activeUser = value;
+                OnPropertyChanged(nameof(ActiveUser));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public bool UserIsActive(string userName)
+        {
+            return false;
+        }
+    }
+}

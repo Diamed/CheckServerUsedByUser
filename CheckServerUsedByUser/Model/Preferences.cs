342546555c46064fe7d924076adba392466fe4dd
Dmitriy Bobrovskiy <apollondiamed@gmail.com>
2018-03-31 13:52:34 +0500

    Added view model and changed preferences to observable collection.

--- CheckServerUsedByUser/Model/Preferences.cs
+++ CheckServerUsedByUser/Model/Preferences.cs
@@ -1,55 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 
 namespace CheckServerUsedByUser.Model
 {
     /// <summary>
     /// All preferences which will contain in application
     /// </summary>
-    internal sealed class Preferences : INotifyPropertyChanged
+    internal sealed class Preferences
     {
-        private IEnumerable<User> _users;
-        public IEnumerable<User> Users
-        {
-            get => _users;
-            set
-            {
-                _users = value;
-                OnPropertyChanged(nameof(Users));
-            }
-        }
-
-        private IEnumerable<Server> _servers;
-        public IEnumerable<Server> Servers
-        {
-            get => _servers;
-            set
-            {
-                _servers = value;
-                OnPropertyChanged(nameof(Servers));
-            }
-        }
-
-        private IEnumerable<LoggedUser> _loggedUsers;
-        public IEnumerable<LoggedUser> LoggedUsers
-        {
-            get => _loggedUsers;
-            set
-            {
-                _loggedUsers = value;
-                OnPropertyChanged(nameof(LoggedUsers));
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged([CallerMemberName] string prop = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
-        }
+        public ObservableCollection<User> Users { get; set; }
+        public ObservableCollection<Server> Servers { get; set; }
+        public ObservableCollection<LoggedUser> LoggedUsers { get; set; }
     }
 }
--- CheckServerUsedByUser/Model/User.cs
+++ CheckServerUsedByUser/Model/User.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+﻿using System.ComponentModel;
 using System.Net;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CheckServerUsedByUser.Model
 {
